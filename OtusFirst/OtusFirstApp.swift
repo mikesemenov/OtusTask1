d6409c560b07c2be69aa36f9d375f6ce18d1d775
Mikhail Semenov <mbsemenov@avito.ru>
2023-09-05 23:30:24 +0400

    Initial Commit

---
+++ OtusFirst/OtusFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  OtusFirstApp.swift
+//  OtusFirst
+//
+//  Created by Mikhail Semenov on 05.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct OtusFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
